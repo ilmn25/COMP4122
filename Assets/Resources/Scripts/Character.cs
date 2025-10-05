d3e825be861687cfef4610fab251f6ece28da146
ilmn25 <kilmn025@gmail.com>
2025-10-05 15:17:25 +0800

    0.31 Basic Character Animation Added At Character.cs

--- Assets/Resources/Scripts/Character.cs
+++ Assets/Resources/Scripts/Character.cs
@@ -1,18 +1,51 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [NonSerialized] public Vector2 Direction;
+    [NonSerialized] public Vector2 Direction; 
     private const int Speed = 5;
     private const int Acceleration = 5;
+    private const int Deceleration = 3;
     private float _currentSpeed;
+    private Vector3 _directionBuffer;
+    private Animator _animator;
+    private GameObject _footprintObject;
+
+    private void Awake()
+    {
+        _footprintObject = UnityEngine.Resources.Load<GameObject>("Prefabs/Footprint");
+        _animator = GetComponent<Animator>();
+        StartCoroutine(FootprintSpawnTimer());
+        return;
+        
+        IEnumerator FootprintSpawnTimer()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+                if (_currentSpeed > 0.1f)
+                {
+                    Instantiate(_footprintObject, transform.position, Quaternion.identity);
+                }
+            } 
+        }
+    }
 
     private void Update()
     {
-        float speedAdjust = Direction.x != 0 && Direction.y != 0 ? 0.7071f * Speed : Speed;
-        _currentSpeed = Mathf.Lerp(_currentSpeed, Direction != Vector2.zero ? speedAdjust : 0, Time.deltaTime * Acceleration);
-        transform.position += new Vector3(Direction.x, Direction.y, 0) * (Time.deltaTime * _currentSpeed); 
+        if (Direction != Vector2.zero)
+        {
+            float speedAdjust = Direction.x != 0 && Direction.y != 0 ? Speed * 0.7071f : Speed;
+            _currentSpeed = Mathf.Lerp(_currentSpeed, speedAdjust, Time.fixedDeltaTime * Acceleration);
+            _directionBuffer =  new Vector3(Direction.x, Direction.y, 0); // deceleration in previous frame's direction
+        }
+        else
+        {
+            _currentSpeed = Mathf.Lerp(_currentSpeed, 0, Time.fixedDeltaTime * Deceleration);
+        } 
+        transform.position += _directionBuffer * (Time.deltaTime * _currentSpeed); 
         
         HandleAnimation();
         Direction = Vector2.zero;
@@ -20,9 +53,22 @@ public class Character : MonoBehaviour
 
     private void HandleAnimation()
     {
-        if (Direction.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+        bool isMoving = Direction != Vector2.zero;
+
+        if (isMoving)
+        {
+            _animator.Play("Move", 0); 
+            _animator.speed = _currentSpeed / Speed;
+        }
+        else
+        {
+            _animator.speed = 1;
+            _animator.Play("Idle", 0);
+        }
+
+        if (Direction.x < 0)    
+            _animator.Play("Left", 1); 
         else if (Direction.x > 0)
-            transform.localScale = new Vector3(1, 1, 1);
+            _animator.Play("Right", 1);
     }
 }
