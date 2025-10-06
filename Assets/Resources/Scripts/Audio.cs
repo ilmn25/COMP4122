e72101867f8ce017020d2c6cbf25dcdd200c8ef3
ilmn25 <kilmn025@gmail.com>
2025-10-06 17:10:25 +0800

    0.8 Cleaned Up Files, Versioned Project Settings, Changed Project Settings

--- Assets/Resources/Scripts/Audio.cs
+++ Assets/Resources/Scripts/Audio.cs
@@ -9,27 +9,26 @@ namespace Resources.Scripts
     }
     public class Audio
     {
-        private static List<AudioSource> _audioSources;
-        private static AudioSource _bgmSource;
+        private static readonly List<AudioSource> AudioSources;
+        private static readonly AudioSource BGMSource;
 
-        private static readonly float BgmVolume = 1f;
-        private static readonly float SfxVolume = 1f;
-        private static readonly int PoolSize = 12;
+        private const float BgmVolume = 1f;
+        private const float SfxVolume = 1f;
+        private const int PoolSize = 12;
 
         private static readonly Dictionary<AudioClipID, float> Volume = new ();
         
         static Audio()
         {
             GameObject audioManager = new GameObject("Audio");
-            _bgmSource = audioManager.AddComponent<AudioSource>();
+            BGMSource = audioManager.AddComponent<AudioSource>();
 
-            _audioSources = new List<AudioSource>();
+            AudioSources = new List<AudioSource>();
             for (int i = 0; i < PoolSize; i++)
             {
                 AudioSource newSource = audioManager.AddComponent<AudioSource>();
-                _audioSources.Add(newSource);
+                AudioSources.Add(newSource);
             } 
-            
             
             Volume[AudioClipID.Noise] = 10;
         }
@@ -37,10 +36,10 @@ namespace Resources.Scripts
         public static void PlayBGM(AudioClipID id, float volume = 1f, bool loop = true)
         {
             AudioClip clip = UnityEngine.Resources.Load<AudioClip>("Audios/" + id);
-            _bgmSource.clip = clip;
-            _bgmSource.volume = volume * BgmVolume;
-            _bgmSource.loop = loop;
-            _bgmSource.Play();
+            BGMSource.clip = clip;
+            BGMSource.volume = volume * BgmVolume;
+            BGMSource.loop = loop;
+            BGMSource.Play();
         }
 
         public static AudioSource PlaySfx(AudioClipID id, bool loop = false)
@@ -61,7 +60,7 @@ namespace Resources.Scripts
 
         public static void StopSfx(AudioSource audioSource)
         {
-            if (audioSource != null)
+            if (audioSource)
             {
                 audioSource.Stop();
                 audioSource.loop = false;
@@ -70,7 +69,7 @@ namespace Resources.Scripts
 
         private static AudioSource GetAvailableAudioSource()
         {
-            foreach (AudioSource source in _audioSources)
+            foreach (AudioSource source in AudioSources)
             {
                 if (!source.isPlaying)
                 {
