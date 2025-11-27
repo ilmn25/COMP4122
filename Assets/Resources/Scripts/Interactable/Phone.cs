c0369f47ee774f362a313d04fad350f89a3f7ce5
ilmn25 <kilmn025@gmail.com>
2025-11-28 03:10:06 +0800

    Phone prefab with progress bar

---
+++ Assets/Resources/Scripts/Interactable/Phone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Resources.Scripts
+{
+    public class Phone : Interactable
+    {
+        private TextMeshPro _text;
+        Coroutine _coroutine;
+        private void Start()
+        {
+            _text = transform.Find("Text").GetComponent<TextMeshPro>();
+        }
+
+        public override void Interact(Character character)
+        {
+            if (_coroutine != null) return;
+            _coroutine = StartCoroutine(Progress()); 
+            IEnumerator Progress()
+            {
+                Audio.PlaySfx(AudioClipID.Item);
+                int progress = 0; 
+                while (true)
+                {
+                    yield return new WaitForSeconds(0.1f);
+                    if (Vector3.Distance(character.transform.position, transform.position) < 2)
+                    {
+                        progress++;
+                        _text.text = progress + "%"; 
+                        if (progress == 100)
+                        {
+                            _coroutine = null;
+                            Debug.Log("win");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        _coroutine = null;
+                        _text.text = "";
+                        break;
+                    }
+                } 
+            }
+        }
+    }
+}
--- Assets/Resources/Scripts/Interactable/Portal.cs
+++ Assets/Resources/Scripts/Interactable/Portal.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
 
 namespace Resources.Scripts
 {
@@ -7,6 +7,19 @@ namespace Resources.Scripts
         public override void Interact(Character character)
         {
             character.transform.position = transform.Find("Destination").position;
+            Audio.PlaySfx(AudioClipID.Item);
+        }
+    }
+
+
+    public class Door : Interactable
+    {
+        public override void Interact(Character character)
+        {
+            if (character.Inventory.Contains((int)ItemID.Card))
+            {
+                character.Inventory.Remove((int)ItemID.Card);
+            }
             Audio.PlaySfx(AudioClipID.Item);
         }
     }
