3861599db836562c7398b8f5722c00139eb8a11e
ilmn25 <kilmn025@gmail.com>
2025-11-29 13:21:26 +0800

    Added Door

---
+++ Assets/Resources/Scripts/Interactable/Door.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Resources.Scripts
+{
+    public class Door : Interactable
+    {
+        public bool isFaceFront;
+        private SpriteRenderer _spriteRenderer;
+        
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.sprite = Cache.LoadSprite(isFaceFront? "Door1" : "Door2");
+        }
+
+        public override void Interact(Character character)
+        {
+            if (character.Inventory.Contains((int)ItemID.Card))
+            {
+                character.Inventory.Remove((int)ItemID.Card);
+                _spriteRenderer.sprite = Cache.LoadSprite(isFaceFront? "Door2": "Door1");
+                transform.Find("Collider").gameObject.SetActive(false);
+            }
+            Audio.PlaySfx(AudioClipID.Item);
+        }
+    }
+}
--- Assets/Resources/Scripts/Interactable/Portal.cs
+++ Assets/Resources/Scripts/Interactable/Portal.cs
@@ -1,5 +1,4 @@
-﻿using Unity.VisualScripting;
-
+﻿
 namespace Resources.Scripts
 {
     public class Portal : Interactable
@@ -7,19 +6,6 @@ namespace Resources.Scripts
         public override void Interact(Character character)
         {
             character.transform.position = transform.Find("Destination").position;
-            Audio.PlaySfx(AudioClipID.Item);
-        }
-    }
-
-
-    public class Door : Interactable
-    {
-        public override void Interact(Character character)
-        {
-            if (character.Inventory.Contains((int)ItemID.Card))
-            {
-                character.Inventory.Remove((int)ItemID.Card);
-            }
             Audio.PlaySfx(AudioClipID.Item);
         }
     }
