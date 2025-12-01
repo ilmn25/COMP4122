03a982bcaab700209c87e76ec6e8ba7b491c04d2
ilmn25 <kilmn025@gmail.com>
2025-12-01 16:53:24 +0800

    Lamp

---
+++ Assets/Resources/Scripts/Interactable/Lamp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine.Rendering.Universal;
+
+namespace Resources.Scripts
+{
+    public class Lamp : Interactable
+    {  
+        private Light2D _light;
+        private readonly NetworkVariable<bool> _on = new ();
+
+        private void Start()
+        { 
+            _light = GetComponent<Light2D>();
+            SetOnServerRpc(_light.enabled);
+            _on.OnValueChanged += (value, newValue) =>
+            { 
+                _light.enabled = newValue;
+            };
+        }
+
+        public override void Interact(Character character)
+        { 
+            SetOnServerRpc(!_on.Value);
+            Audio.PlaySfx(AudioClipID.Item);
+        }
+        
+        [ServerRpc (RequireOwnership = false)]
+        private void SetOnServerRpc(bool on)
+        {
+            _on.Value = on;
+        }
+    }
+}
